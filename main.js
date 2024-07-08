04129c2aa6b0475a8a413d50e4065c1942d61302
Mariyam-Iqbal <mariyambaloch1997@gmail.com>
2024-07-08 05:15:44 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const cfonts_1 = __importDefault(require("cfonts"));
+cfonts_1.default.say("Welcome to|MARIYAM IQBAL|Practice|Reposatory", {
+    font: 'block',
+    align: 'center',
+    colors: ['red', "yellow"],
+    background: 'blackbright',
+    letterSpacing: 1,
+    lineHeight: 1,
+    space: true,
+    maxLength: '0',
+    gradient: false,
+    independentGradient: false,
+    transitionGradient: false,
+    env: 'node'
+});
---
+++ main.ts
@@ -0,0 +1,18 @@
+import cfonts from "cfonts";
+
+cfonts.say("Welcome to|MARIYAM IQBAL|Practice|Reposatory", {
+    font: 'block',
+    align: 'center',
+    colors: ['red', "yellow"],
+    background: 'blackbright',
+    letterSpacing: 1,
+    lineHeight: 1,
+    space: true,
+    maxLength: '0',
+    gradient: false,
+    independentGradient: false,
+    transitionGradient: false,
+    env: 'node'
+  });
+  
+  
